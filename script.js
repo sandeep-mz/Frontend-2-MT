f1f9e1121d987bce980f1e23d856d45380e6e09f
Sandeep Maurya <10millionvibration@gmail.com>
2023-11-04 23:19:49 +0530

    1st commit

--- script.js
+++ script.js
@@ -293,11 +293,7 @@ function defaultCards () {
           </div>
         </div>
         <div class="card__info-last">
-<<<<<<< HEAD
           <h3>${e.time}</h3>
-=======
-          <h3>${e.time} </h3>
->>>>>>> a8f9d73018d11b1a1c8c91cb86a81a965f303807
           <div class="share__container">
             <img src="./assets/like.svg" class="like_img" alt="like" />
             <img src="./assets/comments.svg" alt="like" />
